69ddcf214fdb58ca7672867408832b39c7bbefab
Joshua Tauberer <jt@occams.info>
2016-04-12 08:05:34 -0400

    change version to 0.9.0 for first upload, updated my name/contact info to match what I've put in my other packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='pdf-diff',
-      version='1.0',
+      version='0.9.0',
       description='Finds differences between two PDF documents',
       url='https://github.com/JoshData/pdf-diff',
-      author='JoshData',
-      author_email='',
+      author=u'Joshua Tauberer',
+      author_email=u'jt@occams.info',
       license='CC0 1.0 Universal',
       packages=find_packages(),
       install_requires=[
